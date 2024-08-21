d75d0ad93cfcf9fe2d4bc85ff705004de1a32a0e
BabuBojja <63705627+BabuBojja@users.noreply.github.com>
2024-08-21 12:04:04 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+
+  private int i = 10; 
+  
+  public static void main(String args[]){
+	int j = 20; 
+    System.out.println("Welcome to Demo");
+   }
+  }
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+ 
+ 
+  int j = 20;
+
+   public static void main(String args[]){
+       System.out.println("Welcome to Java");
+     }
+  }
